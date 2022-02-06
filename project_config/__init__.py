8630af365da6a55574cf9a2972ccefccde4824e4
kgpayne <ken@meltano.com>
2022-02-06 00:22:34 +0000

    working gettrs and settrs

--- project_config/__init__.py
+++ project_config/__init__.py
@@ -1,31 +1,75 @@
+from typing import Any
+
+
+class RequiredFieldError(Exception):
+    pass
+
 
 class Base:
+    pass
 
-    def __init__(self, class_):
+
+class BaseField:
+
+    def __init__(self, class_, required=False, default=None):
         self.class_ = class_
+        self.required = required
+        self.default = default
 
 
-class Item(Base):
+class Item(BaseField):
     pass
 
 
-class Array(Base):
+class Array(BaseField):
     pass
+
+
+def property_maker(name):
+
+    @property
+    def prop(self):
+        field = self._model.__dict__.get(name)
+        value = self._dict.get(name)
+
+        if isinstance(field, Item):
+            if Model in field.class_.__bases__:
+                return field.class_.from_dict(value)
+            else:
+                return value
+
+        if isinstance(field, Array):
+            if Model in field.class_.__bases__:
+                return [
+                    field.class_.from_dict(item) for item in value
+                ]
+            else:
+                return [item for item in value]
+
+    @prop.setter
+    def prop(self, value):
+        self._dict[name] = value
+
+    return prop
 
 
 class Model:
 
-    def __init__(self, values: dict):
-        setattr(self, "_dict", values)
-        for attribute, base in self.__class__.__dict__.items():
-            if isinstance(base, Item):
-                setattr(self, attribute, base.class_(values.get(attribute, None)))
-            if isinstance(base, Array):
-                setattr(
-                    self, attribute, [
-                        base.class_(value) for value in values.get(attribute, [])
-                    ]
-                )
+    @classmethod
+    def from_dict(cls, values):
 
-class File(Model):
-    pass
+        class Class(Base): pass
+        Class.__name__ = cls.__name__
+        setattr(Class, "_dict", values)
+        setattr(Class, "_model", cls)
+
+        for prop, field in cls.__dict__.items():
+            if not prop.startswith('__'):
+
+                if prop not in values:
+                    if field.required:
+                        raise RequiredFieldError(f"Required field '{prop}' not found.")
+                else:
+                    setattr(Class, prop, property_maker(prop))
+
+        return Class()
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,8 @@
+import pytest
 import project_config as pc
 
 
-class SettingGroupValidation:
+class SettingGroupValidation(pc.Model):
     pass
 
 
@@ -13,7 +14,7 @@ class Extractor(pc.Model):
     __metadata__ = {
         "additionalProperties": True
     }
-    name = pc.Item(str)
+    name = pc.Item(str, required=True)
     inherit_from = pc.Item(str)
     pip_url = pc.Item(str)
     variant = pc.Item(str)
@@ -35,7 +36,7 @@ class Plugins(pc.Model):
     extractors = pc.Array(Extractor)
 
 
-class ProjectFile(pc.File):
+class ProjectFile(pc.Model):
     __metadata__ = {
         "title": "A Project file.",
         "description": "Project is an Open Source project. Read more at https://github.com/kgpayne/project-config"
@@ -55,6 +56,46 @@ example_project_file_dict = {
     }
 }
 
-def test_dict_persistence():
-    pf = ProjectFile(example_project_file_dict)
+pf = ProjectFile.from_dict(example_project_file_dict)
+
+def test_dict_storage():
     assert pf._dict == example_project_file_dict
+
+def test_properties_created():
+    top_keys = [
+        k for k in list(pf.__class__.__dict__.keys())
+        if not k.startswith('__')
+    ]
+    assert top_keys == [
+        '_dict', '_model', 'plugins'
+    ]
+
+    plugins_keys = [
+        k for k in list(pf.plugins.__class__.__dict__.keys())
+        if not k.startswith('__')
+    ]
+    assert plugins_keys == [
+        '_dict', '_model', 'extractors'
+    ]
+
+    extractor_keys = [
+        k for k in list(pf.plugins.extractors[0].__class__.__dict__.keys())
+        if not k.startswith('__')
+    ]
+    assert extractor_keys == [
+        '_dict', '_model', 'name', 'inherit_from', 'pip_url'
+    ]
+
+
+def test_edit_dict():
+    pf.plugins.extractors[0].name = 'updated-test-extractor'
+    assert example_project_file_dict['plugins']['extractors'][0]['name'] == 'updated-test-extractor'
+
+
+def test_required_field():
+    """ This should raise on instantiation ideally.
+    """
+    example_project_file_dict['plugins']['extractors'][0].pop('name')
+    with pytest.raises(pc.RequiredFieldError):
+        pf = ProjectFile.from_dict(example_project_file_dict)
+        pf.plugins.extractors[0].name
